8679f5603c64c0706765f40c7f989ad92232e1a1
Daniel FCS <danielf.cs@outlook.com>
2023-05-30 15:47:28 +0100

    lets lab

--- 02_mongodb_with_server/mongodb_with_server_games_start/server/helpers/create_router.js
+++ 02_mongodb_with_server/mongodb_with_server_games_start/server/helpers/create_router.js
@@ -1,8 +1,71 @@
 const express = require('express');
+const ObjectID = require('mongodb').ObjectID
 
 const createRouter = function (collection) {
-
+ 
   const router = express.Router();
+
+  router.get("/",(request,response) => {
+    collection
+    .find()
+    .toArray()
+    .then((documents)=> response.json(documents))
+    //to force an error we can change ^ to response.nonsense(documents)
+    .catch((err) => {
+      console.error(err)
+      response.status(500)
+      response.json({status:500,error:err})
+    })
+  })
+
+  router.get('/:id',(request,response) => {
+    const id = request.params.id;
+    collection.findOne({_id:ObjectID(id)})
+    .then(document=> response.json(document))
+    .catch((err) => {
+      console.error(err)
+      response.status(500)
+      response.json({status:500,error:err})
+    })
+  })
+      // response.send("code works") - works as print
+
+    router.post('/',(request,response) => {
+      const newGameData = request.body
+
+      collection
+      .insertOne(newGameData)
+      // .then(()=>collection.find().toArray()) // sends back a confirmation by id
+      .then(dataBaseResponse => response.json(dataBaseResponse.ops[0])) //ops[0] is returning the object we just logged by ID, newer versions will be .insertedID instead of .ops
+      // .then(documents => response.json(documents)) //returns all the games back
+      
+      .catch((err) => {
+        console.error(err)
+        response.status(500)
+        response.json({status:500,error:err})
+      })})
+
+    router.delete('/:id',(request,response) => {
+      const id = request.params.id 
+      collection.deleteOne({_id: ObjectID(id)})
+      .then(result => response.json(result))
+      .catch((err) => {
+        console.error(err)
+        response.status(500)
+        response.json({status:500,error:err})
+      })})
+
+
+    router.put("/:id",(request,response) => {
+      const id = request.params.id
+      const updateData = request.body
+      collection.updateOne({_id:ObjectID(id)},{$set: updateData})
+      .then(result => response.json(result))
+      .catch((err) => {
+        console.error(err)
+        response.status(500)
+        response.json({status:500,error:err})
+      })})
 
   return router;
 
--- 02_mongodb_with_server/mongodb_with_server_games_start/server/server.js
+++ 02_mongodb_with_server/mongodb_with_server_games_start/server/server.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
+//use express
+const MongoClient = require('mongodb').MongoClient;
+//makes connection between MongoDB and Express
+const createRouter = require('./helpers/create_router')
 
 app.use(express.json());
+//parse json data
 
+MongoClient.connect("mongodb://127.0.0.1:27017",{useUnifiedTopology:true}).then((client) => {
+  const db = client.db('games_hub')
+  //if such db does not exist then one will be created
+  const gamesCollection = db.collection('games')
+  const gamesRouter = createRouter(gamesCollection)
+  app.use("/api/games",gamesRouter)
+
+
+}).catch(console.error)
 app.listen(9000, function () {
   console.log(`Listening on port ${ this.address().port }`);
 });
+
